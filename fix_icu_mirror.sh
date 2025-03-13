cdcc78ba96c440e3cde5ffe65d8a8dfa64ab91d9
Henryk Paluch <henryk.paluch@pickering.cz>
2025-03-13 14:59:16 +0100

    Updated fix icu from 1.75 to 1.76

--- fix_icu_mirror.sh
+++ fix_icu_mirror.sh
@@ -2,9 +2,11 @@
 # fix corrupted icu4c mirror (for libreoffice package)
 set -xeuo pipefail
 cd /usr/src/t2-src
-curl -fLo download/mirror/i/icu4c-75_1-data.zip https://github.com/unicode-org/icu/releases/download/release-75-1/icu4c-75_1-data.zip
-file download/mirror/i/icu4c-75_1-data.zip
-rm -f download/mirror/i/icu4c-75_1-data.zip.extck-err
+icuver=76_1
+icuver2=76-1
+curl -fLo download/mirror/i/icu4c-${icuver}-data.zip https://github.com/unicode-org/icu/releases/download/release-${icuver2}/icu4c-${icuver}-data.zip
+file download/mirror/i/icu4c-${icuver}-data.zip
+rm -f download/mirror/i/icu4c-${icuver}-data.zip.extck-err
 # force installation of build deps:
 echo "Run: t2 install libreoffice"
 exit 0
